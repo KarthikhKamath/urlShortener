900544342584330297e0eb2cd3296f28b042d50f
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2023-12-30 16:36:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 
-app.use('/url',restrictToLoggedInUsersOnly, urlRoute)
+app.use('/url', urlRoute)
 app.use('/',checkAuth, staticRouter)
 app.use('/user', userRoute)
 
